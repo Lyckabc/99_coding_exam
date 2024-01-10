d9e5942c380efbcbeda48fb040fff468887e8da4
Lyckabc <dh2heart@gmail.com>
2024-01-10 23:58:08 +0900

    programmers bagic 3day start

--- 00_programmers/코딩테스트 기초/P181942_mixArr/P181942_mixArr.java
+++ 00_programmers/코딩테스트 기초/P181942_mixArr/P181942_mixArr.java
@@ -3,31 +3,38 @@
  **        99_coding_exam                                 **
  **        PACKAGE_NAME                    **
  **        Made by Toji                              **
- **        1/9/2024  11:19 PM                       **
+ **        1/9/2024  11:31 PM                       **
  **        https://github.com/lyckabc                **
  **                                                  **
  ******************************************************/
-package P120813;
+package P181942;
 
 import java.util.*;
 import java.io.*;
 
-public class P120813_hateEvenNum {
+public class P181942_mixArr {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int n = 10;
+        String s1 = "aaaaa";
+        String s2 = "bbbbb";
 
-        System.out.println(sol.solution(n));
+        System.out.println(sol.solution(s1,s2));
     }
 }
 class Solution {
-    public int[] solution(int n) {
-        List<Integer> onlyOdd = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
+    public String solution(String str1, String str2) {
+        String s = "";
+        int j = 0;
+        int k = 0;
+        for (int i = 1; i <= str1.length() + str2.length(); i++) {
             if (i % 2 == 1) {
-                onlyOdd.add(i);
+                s += str1.charAt(j);
+                j++;
+            } else {
+                s += str2.charAt(k);
+                k++;
             }
         }
-        return onlyOdd.stream().mapToInt(i -> i).toArray();
+        return s;
     }
 }
