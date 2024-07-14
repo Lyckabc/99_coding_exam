ccc2fe2fb5ebc5cfcaedd43ca1abb143ea786391
lyckabc <dh2heart@gmail.com>
2024-07-14 17:21:25 +0900

    [level 0] Title: 수 조작하기 2, Time: 16.93 ms, Memory: 99.6 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,26 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public String solution(int[] numLog) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         int lenNum = numLog.length;
+
+        Map<Integer, Character> directionMap = new HashMap<>();
+        directionMap.put(1, 'w');
+        directionMap.put(-1, 's');
+        directionMap.put(10, 'd');
+        directionMap.put(-10, 'a');
+
         for (int i = 1; i < lenNum; i++) {
-            int diff = numLog[i] - numLog[i-1];
-            switch (diff) {
-                case 1:
-                    answer += "w";
-                    break;
-                case -1:
-                    answer += "s";
-                    break;
-                case 10:
-                    answer += "d";
-                    break;
-                case -10:
-                    answer += "a";
-                    break;
-                default:
-                    break;
+            int diff = numLog[i] - numLog[i - 1];
+            Character direction = directionMap.get(diff);
+            if (direction != null) {
+                answer.append(direction);
             }
         }
-        return answer;
+        return answer.toString();
     }
 }
