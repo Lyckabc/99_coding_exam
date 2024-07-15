3a699c95d906159a891ab10e5e456870ea500dfa
lyckabc <dh2heart@gmail.com>
2024-07-16 00:02:26 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.13 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -1,6 +1,5 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Iterator;
 
 class Solution {
     public int solution(int a, int b, int c, int d) {
@@ -9,30 +8,29 @@ class Solution {
         map.put(b, map.getOrDefault(b, 0) + 1);
         map.put(c, map.getOrDefault(c, 0) + 1);
         map.put(d, map.getOrDefault(d, 0) + 1);
-       
+
         int answer = 0;
+
         if (map.size() == 1) {
             answer = 1111 * a;
         } else if (map.size() == 2) {
-            Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
-            Map.Entry<Integer, Integer> first = iterator.next();
-            Map.Entry<Integer, Integer> second = iterator.next();
-            if (first.getValue() == 3 || second.getValue() == 3) {
-                int p = first.getValue() == 3 ? first.getKey() : second.getKey();
-                int q = first.getValue() == 1 ? first.getKey() : second.getKey();
+            int p = 0, q = 0;
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                if (entry.getValue() == 3) {
+                    p = entry.getKey();
+                } else if (entry.getValue() == 1) {
+                    q = entry.getKey();
+                } else if (entry.getValue() == 2) {
+                    p = (p == 0) ? entry.getKey() : p;
+                    q = entry.getKey();
+                }
+            }
+            if (map.containsValue(3)) {
                 answer = (int) Math.pow(10 * p + q, 2);
             } else {
-                int p = first.getKey();
-                int q = second.getKey();
                 answer = (p + q) * Math.abs(p - q);
             }
         } else if (map.size() == 3) {
-            int singleValue = 0;
-            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-                if (entry.getValue() == 2) {
-                    singleValue = entry.getKey();
-                }
-            }
             int product = 1;
             for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
                 if (entry.getValue() == 1) {
@@ -40,10 +38,10 @@ class Solution {
                 }
             }
             answer = product;
-        } else { 
+        } else {
             int minNum = Integer.MAX_VALUE;
-            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-                minNum = Math.min(minNum, entry.getKey());
+            for (int key : map.keySet()) {
+                minNum = Math.min(minNum, key);
             }
             answer = minNum;
         }
