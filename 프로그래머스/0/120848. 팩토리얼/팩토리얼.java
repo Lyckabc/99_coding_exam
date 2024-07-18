c1a59f72974da9d6f0ad116ecdd5607ff8ccc57e
lyckabc <dh2heart@gmail.com>
2024-07-18 23:23:20 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -2,12 +2,9 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         while (Factorial(answer) <= n) {
-            if (Factorial(answer + 1) > n) {
-                return answer;
-            }
             answer++;
         }
-        return answer;
+        return answer - 1;
     }
     public int Factorial(int n) {
         if (n == 0) return 1;
