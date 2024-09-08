ea8debd536cf0d6ea427f6bdf1fc49c9e057580a
lyckabc <dh2heart@gmail.com>
2024-09-09 00:05:52 +0900

    P_CT_foundation_day21 Prac4 fini

--- programmers/CT_foundation/day21/P181849/P181849.java
+++ programmers/CT_foundation/day21/P181849/P181849.java
@@ -10,13 +10,10 @@ package programmers.CT_foundation.day21.P181849;
 public class P181849 {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int a = 1;
-        int[] A = {1, 2, 3};
-        String s = "abc";
+        String num_str = "123456789";
 
         long startTime = System.nanoTime();
-        System.out.println(sol.solution(A));
-        System.out.println(sol.solution(s));
+        System.out.println(sol.solution(num_str));
         long endTime = System.nanoTime();
         System.out.printf("method speed: %.6f ms%n", (endTime - startTime) / 1_000_000.0);
 
@@ -26,6 +23,32 @@ public class P181849 {
 class Solution {
     public int solution(String num_str) {
         int answer = 0;
+        for (int i = 0; i < num_str.length(); i++) {
+            char c = num_str.charAt(i);
+            if (Character.isDigit(c)) {
+                int tmpNum = c - '0';
+                answer += tmpNum;
+            }
+        }
         return answer;
     }
 }
+/*
+class Solution {
+    public int solution(String numStr) {
+        return numStr.chars().map(c -> c - 48).sum();
+    }
+}
+ */
+/*
+class Solution {
+    public int solution(String num_str) {
+        int answer = 0;
+        String[] numArr= num_str.split("");
+        for (String num : numArr){
+            answer+=Integer.parseInt(num);
+        }
+        return answer;
+    }
+}
+ */
